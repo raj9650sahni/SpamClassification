d42a540f1ea14bf44051d292e0d8379938103d47
Raj Kishore Sahni <63728002+raj9650sahni@users.noreply.github.com>
2021-04-17 16:02:01 +0530

    Update api.py

--- api.py
+++ api.py
@@ -6,7 +6,6 @@ import pandas as pd
 import pickle
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
-import sklearn.externals
 import joblib
 
 app = flask.Flask(__name__)
